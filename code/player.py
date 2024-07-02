d4d2a42a8524b4cc42fafe4408bc2e73552c033a
spazapy <n.marcel.1994@gmx.de>
2024-07-02 09:13:31 +0200

    added another movement option

--- code/player.py
+++ code/player.py
@@ -111,18 +111,18 @@ class Player(pygame.sprite.Sprite):
 
         #directions
         if not self.timers["tool use"].active and not self.sleep:
-            if keys[pygame.K_UP]:
+            if keys[pygame.K_UP] or keys[pygame.K_w]:
                 self.direction.y = -1
                 self.status = "up"
-            elif keys[pygame.K_DOWN]:
+            elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
                 self.direction.y = 1
                 self.status = "down"
             else:
                 self.direction.y = 0
-            if keys[pygame.K_RIGHT]:
+            if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 self.direction.x = 1
                 self.status = "right"
-            elif keys[pygame.K_LEFT]:
+            elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 self.direction.x = -1
                 self.status = "left"
             else:
